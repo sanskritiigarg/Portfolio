d2a69a7bca468e922ac11bd1b12c473e8c40c4e9
Sanskriti Garg <sanskritii.garg@gmail.com>
2026-03-05 01:00:18 +0530

    add animated ui

--- components/home/hero/HeroSection.tsx
+++ components/home/hero/HeroSection.tsx
@@ -3,27 +3,52 @@ import Image from "next/image";
 import AnimatedIntro from "./AnimatedIntro";
 import LinkButton from "@/components/general/LinkButton";
 import { FaArrowRight } from "react-icons/fa6";
-import Projects from "@/components/projects/Projects";
+import Particles from "@/components/home/Particles";
 
 function HeroSection() {
   return (
-    <section id="home" className="h-screen relative flex justify-center items-center overflow-hidden flex-col">
-        
+    <section
+      id="home"
+      className="h-screen relative flex justify-center items-center overflow-hidden flex-col"
+    >
+      <div style={{ width: "100%", height: "600px", position: "relative" }}>
+        <Particles
+          particleColors={["#ffffff"]}
+          particleCount={300}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover
+          alphaParticles={false}
+          disableRotation={false}
+          pixelRatio={1}
+        />
+      </div>
       <div className="absolute z-10 flex flex-col items-center">
-
         {/* image */}
         <div className="relative w-36 h-36 rounded-full border-2 border-accent">
-          <div className="absolute inset-0 rounded-full blur-lg animate-pulse opacity-60 bg-linear-to-r from-accent to-primary/80 w-40 h-40"/>
-          <Image src="/my-photo.jpeg" alt="My picture" className="rounded-full object-cover" fill/>
+          <div className="absolute inset-0 rounded-full blur-lg animate-pulse opacity-60 bg-linear-to-r from-accent to-primary/80 w-40 h-40" />
+          <Image
+            src="/my-photo.jpeg"
+            alt="My picture"
+            className="rounded-full object-cover"
+            fill
+          />
         </div>
 
-        <h1 className="py-2 my-4 text-center text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold tracking-wide text-transparent bg-clip-text bg-linear-to-r to-primary from-surface">Hi, I am Sanskriti Garg</h1>
-        <AnimatedIntro/>
-        <LinkButton href="#projects" icon={FaArrowRight} iconPosition="right" text="See my work"/>
+        <h1 className="py-2 my-4 text-center text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold tracking-wide text-transparent bg-clip-text bg-linear-to-r to-primary from-surface">
+          Hi, I am Sanskriti Garg
+        </h1>
+        <AnimatedIntro />
+        <LinkButton
+          href="#projects"
+          icon={FaArrowRight}
+          iconPosition="right"
+          text="See my work"
+        />
       </div>
-
     </section>
-  )
+  );
 }
 
 export default HeroSection;
