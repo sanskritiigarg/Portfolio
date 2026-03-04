1ba046a9f673dbcc553687216352cdcdbb8fc9f2
Sanskriti Garg <sanskritii.garg@gmail.com>
2026-03-04 17:11:54 +0530

    add footer

---
+++ components/general/Footer.tsx
@@ -0,0 +1,13 @@
+
+
+function Footer() {
+  return (
+    <footer className="border-t py-8">
+      <div className="px-4 text-center text-base">
+        &copy; {new Date().getFullYear()} Sanskriti
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
